2ce1d294b80984114af0f02c60bf730da17350f5
Nathaniel <nathaniellabrecque8@hotmail.com>
2021-03-10 11:36:19 -0500

    .

--- TP2/algo/main.cpp
+++ TP2/algo/main.cpp
@@ -1,14 +1,19 @@
 #include "allAlgo.h"
+#include <time.h>
 
 int main(int argc, char * argv[])
 {
     vector<XY> points = readFile("../data/N1000_0"); // argv[1]);
     vector<XY> result;
+    clock_t start;
+    clock_t end;
 
     switch ('0') // *argv[2])
     {
     case '0':
+        start = clock();
         result = glouton(points);
+        end = clock();
         break;
     
     case '1':
@@ -21,6 +26,12 @@ int main(int argc, char * argv[])
     
     default:
         break;
+    }
+
+    if(true) // *argv[3] == '1')
+    {
+        double cpuTimeUsed = ((double) (end - start)) / CLOCKS_PER_SEC * 1000;
+        cout << cpuTimeUsed << endl;
     }
 
     if(true) // *argv[4] == '1')
