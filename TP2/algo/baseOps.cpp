838284af5101dc889a0a0a71851ef61ad5f36d17
Nathaniel <nathaniellabrecque8@hotmail.com>
2021-03-10 10:23:19 -0500

    .

---
+++ TP2/algo/baseOps.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+typedef struct
+{
+    int x;
+    int y;
+} XY;
+
+vector<XY> readFile(string path)
+{
+    ifstream myReadFile;
+    string line;
+    vector<XY> point;
+    bool first = true;
+
+    myReadFile.open(path);
+    if (myReadFile.is_open()) {
+        while (!myReadFile.eof()) {
+            myReadFile >> line;
+            if(!first) 
+            {
+                XY xy;
+                xy.x = stoi(line);
+                myReadFile >> line;
+                xy.y = stoi(line);
+
+                point.push_back(xy);
+            }
+            first = false;
+        }
+    }
+    
+    myReadFile.close();  
+
+    point.pop_back();
+      
+    return point;
+}
