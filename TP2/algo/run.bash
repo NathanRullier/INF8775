26050605bef92d6486ff6f40ffed71f369894dea
Nathaniel <nathaniellabrecque8@hotmail.com>
2021-03-31 12:11:28 -0400

    ...

--- TP2/algo/run.bash
+++ TP2/algo/run.bash
@@ -1,12 +1,13 @@
-echo "algo,serie,taille,temps" > ./results.csv
+echo "algo,serie,taille,temps,distance" > ./results.csv
 
-ex_folder="data"
+ex_folder="../data"
 
-for algo in {"progdyn"}; do
+for algo in "progdyn"; do
     for ex in $(ls ${ex_folder}/dp); do
         size=$(echo $ex | cut -d_ -f2)
-        t=$(./tp.sh -e ./${ex_folder}/${testset_folder}/${ex} -a $algo -t)
-        echo $algo,$testset_folder,$size,$t
+        t=$(./tp.sh -e ${ex_folder}/dp/${ex} -a $algo -t)
+        d=$(./tp.sh -e ${ex_folder}/dp/${ex} -a $algo -d)
+        echo $algo,"dp",$size,$t,$d
     done
 done >> results.csv
 
@@ -14,8 +15,9 @@ for algo in {"glouton", "approx"}; do
     for testset_folder in $(ls $ex_folder); do
         for ex in $(ls ${ex_folder}/${testset_folder}); do
             size=$(echo $ex | cut -d_ -f2)
-            t=$(./tp.sh -e ./${ex_folder}/${testset_folder}/${ex} -a $algo -t)
-            echo $algo,$testset_folder,$size,$t
+            t=$(./tp.sh -e ${ex_folder}/${testset_folder}/${ex} -a $algo -t)
+            d=$(./tp.sh -e ${ex_folder}/${testset_folder}/${ex} -a $algo -d)
+            echo $algo,$testset_folder,$size,$t,$d
         done
     done
 done >> results.csv
