e5273729ee2f4a9bb7e4906fd67bae46a2e69981
Nathan Rullier <narul@l4714-02.info.polymtl.ca>
2021-04-14 15:44:43 -0400

    calcul tout le profit

--- TP3/algo/baseOps.cpp
+++ TP3/algo/baseOps.cpp
@@ -1,7 +1,7 @@
-#include "baseOps.h"
+    #include "baseOps.h"
 
-int readFile(string const path, std::vector<std::vector<int>>& value,std::vector<std::vector<int>>& cost)
-{
+    int readFile(string const path, std::vector<std::vector<int>> &value, std::vector<std::vector<int>> &cost)
+    {
     int n = 0;
     int m = 0;
     ifstream myReadFile;
@@ -9,21 +9,27 @@ int readFile(string const path, std::vector<std::vector<int>>& value,std::vector
     bool first = true;
 
     myReadFile.open(path);
-    if (myReadFile.is_open()) {
-        for(int z = 0; z < 2; z++){
-            if(!first)
+    if (myReadFile.is_open())
+    {
+        for (int z = 0; z < 2; z++)
+        {
+            if (!first)
             {
-                std::vector<std::vector<int>> valueRead(n,std::vector<int>(m));
-                std::vector<std::vector<int>> costRead(n,std::vector<int>(m));
+                std::vector<std::vector<int>> valueRead(n, std::vector<int>(m));
+                std::vector<std::vector<int>> costRead(n, std::vector<int>(m));
 
-                for(int i = 0; i < n; i++){
-                    for(int j = 0; j < n; j++){
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
                         myReadFile >> line;
                         valueRead[i][j] = stoi(line);
                     }
                 }
-                for(int i = 0; i < n; i++){
-                    for(int j = 0; j < n; j++){
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
                         myReadFile >> line;
                         costRead[i][j] = stoi(line);
                     }
@@ -31,7 +37,8 @@ int readFile(string const path, std::vector<std::vector<int>>& value,std::vector
                 value = valueRead;
                 cost = costRead;
             }
-            if(first){
+            if (first)
+            {
                 first = false;
                 myReadFile >> line;
                 n = stoi(line);
@@ -40,40 +47,90 @@ int readFile(string const path, std::vector<std::vector<int>>& value,std::vector
             }
         }
     }
-    
+
     myReadFile.close();
 
     return 0;
-}
+    }
 
-int value_profit(int x, int y,std::vector<std::vector<int>> profit, 
-std::vector<std::vector<bool>> & diggedUp, std::vector<std::vector<bool>> diggedUpCopy){
+    int value_profit(int x, int y, std::vector<std::vector<int>> profit,
+                std::vector<std::vector<bool>> &diggedUp, std::vector<std::vector<bool>> diggedUpCopy)
+    {
     int total_profit = 0;
     int left = x;
-    int right =x;
+    int right = x;
     int y2 = y;
-    do{
-        for(int i = left; i < right +1; i++){
-            if(diggedUp[y][i] == false){
+    do
+    {
+        for (int i = left; i < right + 1; i++)
+        {
+            if (diggedUp[y][i] == false)
+            {
                 total_profit += profit[y][i];
                 diggedUpCopy[y][i] = true;
             }
         }
-        if(left != 0){
+        if (left != 0)
+        {
             left--;
         }
-        if(right != profit.size()-1){
+        if (right != profit.size() - 1)
+        {
             right++;
         }
 
         y--;
-    }while(y >= 0);
+    } while (y >= 0);
 
-
-    if(total_profit>0){
+    if (total_profit > 0)
+    {
         diggedUp = diggedUpCopy;
         // cout << x << " " << y2 << " " << total_profit << endl;
     }
 
     return total_profit;
-}
+    }
+
+    std::vector<std::vector<int>> all_profit(std::vector<std::vector<int>> profit, std::vector<std::vector<bool>> &diggedUp)
+    {
+
+    std::vector<std::vector<int>> total_profits(profit.size(), std::vector<int>(profit[0].size()));
+    int max_profit = 0;
+    int max_profit_i = 0;
+    int max_profit_j = 0;
+
+    int profit_size = profit.size();
+
+    for (int i = 0; i < profit_size; i++)
+    {
+        for (int j = 0; j < profit_size; j++)
+        {
+            if (diggedUp[i][j] == false)
+            {
+
+                total_profits[i][j] = profit[i][j];
+                if (i != 0)
+                {
+                    int left = (j == 0) ? j : j - 1;
+                    int right = (j == profit_size - 1) ? j : j + 1;
+                    for (int z = left; z < right + 1; z++)
+                    {
+
+                        if (diggedUp[i][z] == false)
+                        {
+                            total_profits[i][j] += profit[i - 1][z];
+                        
+                            if (total_profits[i][j] > max_profit)
+                            {
+                                int max_profit = total_profits[i][j];
+                                int max_profit_i = i;
+                                int max_profit_j = j;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return total_profits;
+    }
--- TP3/algo/baseOps.h
+++ TP3/algo/baseOps.h
@@ -7,3 +7,5 @@ using namespace std;
 int readFile(string const path, std::vector<std::vector<int>>& value,std::vector<std::vector<int>>& cost);
 
 int value_profit(int x, int y,std::vector<std::vector<int>> profit, std::vector<std::vector<bool>> & diggedUp, std::vector<std::vector<bool>> diggedUpCopy);
+
+std::vector<std::vector<int>> all_profit(std::vector<std::vector<int>> profit, std::vector<std::vector<bool>> & diggedUp);
